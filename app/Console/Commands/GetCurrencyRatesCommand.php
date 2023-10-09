19c3a78281a44df1ab5c32c5d98275867d99890a
anton.chernets <anton.chernets@artpix3d.com>
2023-10-09 17:46:03 +0300

    refactor exchange to job horizon

---
+++ app/Console/Commands/GetCurrencyRatesCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\GetCurrencyRatesJob;
+use App\Services\Currency\CurrencyService;
+use Illuminate\Console\Command;
+
+class GetCurrencyRatesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get-currency-exchanges';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        GetCurrencyRatesJob::dispatch(
+            CurrencyService::updateOrCreateCurrency('UAH')
+        );
+    }
+}
---
+++ app/Jobs/GetCurrencyRatesJob.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Currency;
+use App\Services\Currency\CurrencyService;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class GetCurrencyRatesJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Currency $currency,
+    ) {}
+
+    /**
+     * Execute the job.
+     * @throws GuzzleException
+     */
+    public function handle(Client $httpClient): void
+    {
+        $json = $httpClient->request(
+            'GET',
+            'https://api.forexrateapi.com/v1/latest?api_key=19f9b5894c0764aef905c785b328b94e&base=' . $this->currency->slug
+        )
+            ->getBody()
+            ->getContents();//TODO method request-or
+
+        $result = json_decode($json, true);
+
+        if (isset($result['base']) && isset($result['rates'])) {//TODO method service
+            foreach ($result['rates'] as $key => $rate) {
+                $currency = CurrencyService::updateOrCreateCurrency($key);
+                $this->currency->rates()->attach($currency->id, ['rate_value' => $rate]);
+            }
+        }
+    }
+}
