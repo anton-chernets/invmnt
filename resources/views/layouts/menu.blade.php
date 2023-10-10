7ed6ddc79dab778534b7ec2051353c442324448c
anton.chernets <anton.chernets@artpix3d.com>
2023-10-10 12:33:17 +0300

    fixed menu items

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,21 @@
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
+</li>
 
-
-    <a href="{{ route('user.rates') }}" class="nav-link {{ Request::is('user.rates') ? 'active' : '' }}">
+<li class="nav-item">
+    <a href="{{ route('user.rates') }}" class="nav-link {{ Request::is('user/rates') ? 'active' : '' }}">
         <i class="nav-icon fas fa-tachometer-alt"></i>
         <p>Currency Rates</p>
     </a>
+</li>
 
-    @can('horizon')
-        <a href="{{ route('horizon.index') }}" class="nav-link {{ Request::is('horizon.index') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
+@can('horizon')
+    <li class="nav-item">
+        <a href="{{ route('horizon.index') }}" class="nav-link">
             <p>Horizon dashboard</p>
         </a>
-    @endif
-</li>
+    </li>
+@endif
