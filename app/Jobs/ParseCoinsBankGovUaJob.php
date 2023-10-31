4915ffbac2f96547175bae577ea03cd3b4d44729
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-10-31 17:43:21 +0200

    creates basic job

---
+++ app/Jobs/ParseCoinsBankGovUaJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Coin\ParseCoinsBankGovUaService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ParseCoinsBankGovUaJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        /** @var ParseCoinsBankGovUaService $parser */
+        $parser = app(ParseCoinsBankGovUaService::class);
+        $parser->parseCoinsData(endPage: 2);
+    }
+}
