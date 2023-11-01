c0732e40511f1b16e3db31b48c0042df82fd316f
anton.chernets <anton.chernets@artpix3d.com>
2023-11-01 14:33:17 +0200

    added parse:coins_bank_gov_ua to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->command('app:get-currency-exchanges')->hourly();
+         $schedule->command('parse:coins_bank_gov_ua')->hourly();
     }
 
     /**
