77564cb4aa56fee9f78aaa4e06042f2ecda4d289
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-06 16:58:41 +0200

    get link, send it, delete cache

--- app/Observers/CoinObserver.php
+++ app/Observers/CoinObserver.php
@@ -5,21 +5,31 @@ namespace App\Observers;
 use App\Events\CoinCreated;
 use App\Events\CoinUpdated;
 use App\Models\Coin;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Event;
 
 class CoinObserver
 {
     public function created(Coin $coin): void
     {
-        Event::dispatch(new CoinCreated($coin->name, $coin->count));
+        $coinPageURL = Cache::get("coin_page_url_{$coin->name}");
+
+        Event::dispatch(new CoinCreated($coin->name, $coin->count, $coinPageURL));
+
+        Cache::forget("coin_page_url_{$coin->name}");
     }
+
     public function updated(Coin $coin): void
     {
+        $coinPageURL = Cache::get("coin_page_url_{$coin->count}");
+
         if ($coin->isDirty('count')) {
             $oldCount = $coin->getOriginal('count');
             if ((is_null($oldCount) || $oldCount === 0) && $coin->count > 1) {
-                event(new CoinUpdated($coin->name, $coin->count, $oldCount));
+                event(new CoinUpdated($coin->name, $coin->count, $oldCount, $coinPageURL));
             }
         }
+
+        Cache::forget("coin_page_url_{$coin->count}");
     }
 }
