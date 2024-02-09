4055e2bfdb3b2cbc75b33b3859a4f402743d6056
anton.chernets <anton.chernets@artpix3d.com>
2024-02-09 16:18:22 +0200

    fixed name on null

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -25,7 +25,7 @@ class UserResource extends JsonResource
         return [
             'name' => $this->resource->name,
             'email' => $this->resource->email,
-            'role' => $this->resource->roles?->first()->name,
+            'role' => $this->resource->roles->first()?->name,
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
         ];
