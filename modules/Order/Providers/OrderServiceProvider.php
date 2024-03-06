47f835b98b9598a8f1b7a79f5abe99e984df22ba
anton.chernets <anton.chernets@artpix3d.com>
2024-03-06 18:07:39 +0200

    added views and components to the modules

--- modules/Order/Providers/OrderServiceProvider.php
+++ modules/Order/Providers/OrderServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Order\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class OrderServiceProvider extends ServiceProvider
@@ -9,6 +10,9 @@ class OrderServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadViewsFrom(__DIR__ . '/../Views', 'order');
+
+        Blade::anonymousComponentPath(__DIR__ . '/../Views/components', 'order');
 
         $this->app->register(RouteServiceProvider::class);
         $this->app->register(EventServiceProvider::class);
---
+++ modules/Order/Views/checkout/index.blade.php
@@ -0,0 +1,5 @@
+<div>
+    Hello
+
+    <x-order::order-line product="Test product"/>
+</div>
